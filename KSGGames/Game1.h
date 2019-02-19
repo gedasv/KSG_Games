4d5dd22abd72e7fb00804f8c1443abca74fd59ae
MyRuonis <kciulcinskas@gmail.com>
2019-02-19 17:45:53 +0200

    All Chess pieces

--- KSGGames/Game1.h
+++ KSGGames/Game1.h
@@ -32,18 +32,12 @@ namespace KSGGames {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::PictureBox^  black_bishop_l;
-	private: System::Windows::Forms::PictureBox^  black_bishop_r;
 	protected:
 
 
 	private: System::Windows::Forms::PictureBox^  black_horse_l;
 	private: System::Windows::Forms::PictureBox^  black_rook_l;
 	private: System::Windows::Forms::PictureBox^  black_knight_r;
-
-
-
-
 	private: System::Windows::Forms::PictureBox^  black_queen;
 	private: System::Windows::Forms::PictureBox^  black_king;
 	private: System::Windows::Forms::PictureBox^  black_rook_r;
@@ -55,6 +49,21 @@ namespace KSGGames {
 	private: System::Windows::Forms::PictureBox^  black_pawn_6;
 	private: System::Windows::Forms::PictureBox^  black_pawn_7;
 	private: System::Windows::Forms::PictureBox^  black_pawn_8;
+	private: System::Windows::Forms::PictureBox^  black_bishop_l;
+	private: System::Windows::Forms::PictureBox^  black_bishop_r;
+	private: System::Windows::Forms::PictureBox^  white_pawn_1;
+	private: System::Windows::Forms::PictureBox^  white_pawn_2;
+	private: System::Windows::Forms::PictureBox^  white_pawn_3;
+	private: System::Windows::Forms::PictureBox^  white_pawn_4;
+	private: System::Windows::Forms::PictureBox^  white_pawn_5;
+	private: System::Windows::Forms::PictureBox^  white_pawn_6;
+	private: System::Windows::Forms::PictureBox^  white_pawn_7;
+	private: System::Windows::Forms::PictureBox^  white_pawn_8;
+	private: System::Windows::Forms::PictureBox^  white_rook_l;
+	private: System::Windows::Forms::PictureBox^  white_knight_l;
+	private: System::Windows::Forms::PictureBox^  white_bishop_l;
+	private: System::Windows::Forms::PictureBox^  white_queen;
+	private: System::Windows::Forms::PictureBox^  white_bishop_r;
 
 
 
@@ -62,6 +71,15 @@ namespace KSGGames {
 
 
 
+
+
+
+
+
+
+	private: System::Windows::Forms::PictureBox^  white_king;
+	private: System::Windows::Forms::PictureBox^  white_knight_r;
+	private: System::Windows::Forms::PictureBox^  white_rook_r;
 
 
 
@@ -104,6 +122,22 @@ namespace KSGGames {
 			this->black_pawn_6 = (gcnew System::Windows::Forms::PictureBox());
 			this->black_pawn_7 = (gcnew System::Windows::Forms::PictureBox());
 			this->black_pawn_8 = (gcnew System::Windows::Forms::PictureBox());
+			this->white_pawn_1 = (gcnew System::Windows::Forms::PictureBox());
+			this->white_pawn_2 = (gcnew System::Windows::Forms::PictureBox());
+			this->white_pawn_3 = (gcnew System::Windows::Forms::PictureBox());
+			this->white_pawn_4 = (gcnew System::Windows::Forms::PictureBox());
+			this->white_pawn_5 = (gcnew System::Windows::Forms::PictureBox());
+			this->white_pawn_6 = (gcnew System::Windows::Forms::PictureBox());
+			this->white_pawn_7 = (gcnew System::Windows::Forms::PictureBox());
+			this->white_pawn_8 = (gcnew System::Windows::Forms::PictureBox());
+			this->white_rook_l = (gcnew System::Windows::Forms::PictureBox());
+			this->white_knight_l = (gcnew System::Windows::Forms::PictureBox());
+			this->white_bishop_l = (gcnew System::Windows::Forms::PictureBox());
+			this->white_queen = (gcnew System::Windows::Forms::PictureBox());
+			this->white_bishop_r = (gcnew System::Windows::Forms::PictureBox());
+			this->white_king = (gcnew System::Windows::Forms::PictureBox());
+			this->white_knight_r = (gcnew System::Windows::Forms::PictureBox());
+			this->white_rook_r = (gcnew System::Windows::Forms::PictureBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->black_bishop_l))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->black_bishop_r))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->black_horse_l))->BeginInit();
@@ -120,6 +154,22 @@ namespace KSGGames {
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->black_pawn_6))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->black_pawn_7))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->black_pawn_8))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_3))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_4))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_5))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_6))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_7))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_8))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_rook_l))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_knight_l))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_bishop_l))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_queen))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_bishop_r))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_king))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_knight_r))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_rook_r))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// black_bishop_l
@@ -282,12 +332,188 @@ namespace KSGGames {
 			this->black_pawn_8->TabIndex = 20;
 			this->black_pawn_8->TabStop = false;
 			// 
+			// white_pawn_1
+			// 
+			this->white_pawn_1->BackColor = System::Drawing::Color::Transparent;
+			this->white_pawn_1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_pawn_1.BackgroundImage")));
+			this->white_pawn_1->Location = System::Drawing::Point(35, 455);
+			this->white_pawn_1->Name = L"white_pawn_1";
+			this->white_pawn_1->Size = System::Drawing::Size(70, 70);
+			this->white_pawn_1->TabIndex = 21;
+			this->white_pawn_1->TabStop = false;
+			// 
+			// white_pawn_2
+			// 
+			this->white_pawn_2->BackColor = System::Drawing::Color::Transparent;
+			this->white_pawn_2->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_pawn_2.BackgroundImage")));
+			this->white_pawn_2->Location = System::Drawing::Point(105, 455);
+			this->white_pawn_2->Name = L"white_pawn_2";
+			this->white_pawn_2->Size = System::Drawing::Size(70, 70);
+			this->white_pawn_2->TabIndex = 22;
+			this->white_pawn_2->TabStop = false;
+			// 
+			// white_pawn_3
+			// 
+			this->white_pawn_3->BackColor = System::Drawing::Color::Transparent;
+			this->white_pawn_3->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_pawn_3.BackgroundImage")));
+			this->white_pawn_3->Location = System::Drawing::Point(175, 455);
+			this->white_pawn_3->Name = L"white_pawn_3";
+			this->white_pawn_3->Size = System::Drawing::Size(70, 70);
+			this->white_pawn_3->TabIndex = 23;
+			this->white_pawn_3->TabStop = false;
+			// 
+			// white_pawn_4
+			// 
+			this->white_pawn_4->BackColor = System::Drawing::Color::Transparent;
+			this->white_pawn_4->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_pawn_4.BackgroundImage")));
+			this->white_pawn_4->Location = System::Drawing::Point(245, 455);
+			this->white_pawn_4->Name = L"white_pawn_4";
+			this->white_pawn_4->Size = System::Drawing::Size(70, 70);
+			this->white_pawn_4->TabIndex = 24;
+			this->white_pawn_4->TabStop = false;
+			// 
+			// white_pawn_5
+			// 
+			this->white_pawn_5->BackColor = System::Drawing::Color::Transparent;
+			this->white_pawn_5->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_pawn_5.BackgroundImage")));
+			this->white_pawn_5->Location = System::Drawing::Point(315, 455);
+			this->white_pawn_5->Name = L"white_pawn_5";
+			this->white_pawn_5->Size = System::Drawing::Size(70, 70);
+			this->white_pawn_5->TabIndex = 25;
+			this->white_pawn_5->TabStop = false;
+			// 
+			// white_pawn_6
+			// 
+			this->white_pawn_6->BackColor = System::Drawing::Color::Transparent;
+			this->white_pawn_6->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_pawn_6.BackgroundImage")));
+			this->white_pawn_6->Location = System::Drawing::Point(385, 455);
+			this->white_pawn_6->Name = L"white_pawn_6";
+			this->white_pawn_6->Size = System::Drawing::Size(70, 70);
+			this->white_pawn_6->TabIndex = 26;
+			this->white_pawn_6->TabStop = false;
+			// 
+			// white_pawn_7
+			// 
+			this->white_pawn_7->BackColor = System::Drawing::Color::Transparent;
+			this->white_pawn_7->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_pawn_7.BackgroundImage")));
+			this->white_pawn_7->Location = System::Drawing::Point(455, 455);
+			this->white_pawn_7->Name = L"white_pawn_7";
+			this->white_pawn_7->Size = System::Drawing::Size(70, 70);
+			this->white_pawn_7->TabIndex = 27;
+			this->white_pawn_7->TabStop = false;
+			// 
+			// white_pawn_8
+			// 
+			this->white_pawn_8->BackColor = System::Drawing::Color::Transparent;
+			this->white_pawn_8->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_pawn_8.BackgroundImage")));
+			this->white_pawn_8->Location = System::Drawing::Point(525, 455);
+			this->white_pawn_8->Name = L"white_pawn_8";
+			this->white_pawn_8->Size = System::Drawing::Size(70, 70);
+			this->white_pawn_8->TabIndex = 28;
+			this->white_pawn_8->TabStop = false;
+			// 
+			// white_rook_l
+			// 
+			this->white_rook_l->BackColor = System::Drawing::Color::Transparent;
+			this->white_rook_l->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_rook_l.BackgroundImage")));
+			this->white_rook_l->Location = System::Drawing::Point(35, 525);
+			this->white_rook_l->Name = L"white_rook_l";
+			this->white_rook_l->Size = System::Drawing::Size(70, 70);
+			this->white_rook_l->TabIndex = 29;
+			this->white_rook_l->TabStop = false;
+			// 
+			// white_knight_l
+			// 
+			this->white_knight_l->BackColor = System::Drawing::Color::Transparent;
+			this->white_knight_l->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_knight_l.BackgroundImage")));
+			this->white_knight_l->Location = System::Drawing::Point(105, 525);
+			this->white_knight_l->Name = L"white_knight_l";
+			this->white_knight_l->Size = System::Drawing::Size(70, 70);
+			this->white_knight_l->TabIndex = 30;
+			this->white_knight_l->TabStop = false;
+			// 
+			// white_bishop_l
+			// 
+			this->white_bishop_l->BackColor = System::Drawing::Color::Transparent;
+			this->white_bishop_l->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_bishop_l.BackgroundImage")));
+			this->white_bishop_l->Location = System::Drawing::Point(175, 525);
+			this->white_bishop_l->Name = L"white_bishop_l";
+			this->white_bishop_l->Size = System::Drawing::Size(70, 70);
+			this->white_bishop_l->TabIndex = 31;
+			this->white_bishop_l->TabStop = false;
+			// 
+			// white_queen
+			// 
+			this->white_queen->BackColor = System::Drawing::Color::Transparent;
+			this->white_queen->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_queen.BackgroundImage")));
+			this->white_queen->Location = System::Drawing::Point(245, 525);
+			this->white_queen->Name = L"white_queen";
+			this->white_queen->Size = System::Drawing::Size(70, 70);
+			this->white_queen->TabIndex = 32;
+			this->white_queen->TabStop = false;
+			// 
+			// white_bishop_r
+			// 
+			this->white_bishop_r->BackColor = System::Drawing::Color::Transparent;
+			this->white_bishop_r->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_bishop_r.BackgroundImage")));
+			this->white_bishop_r->Location = System::Drawing::Point(385, 525);
+			this->white_bishop_r->Name = L"white_bishop_r";
+			this->white_bishop_r->Size = System::Drawing::Size(70, 70);
+			this->white_bishop_r->TabIndex = 33;
+			this->white_bishop_r->TabStop = false;
+			// 
+			// white_king
+			// 
+			this->white_king->BackColor = System::Drawing::Color::Transparent;
+			this->white_king->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_king.BackgroundImage")));
+			this->white_king->Location = System::Drawing::Point(315, 525);
+			this->white_king->Name = L"white_king";
+			this->white_king->Size = System::Drawing::Size(70, 70);
+			this->white_king->TabIndex = 33;
+			this->white_king->TabStop = false;
+			// 
+			// white_knight_r
+			// 
+			this->white_knight_r->BackColor = System::Drawing::Color::Transparent;
+			this->white_knight_r->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_knight_r.BackgroundImage")));
+			this->white_knight_r->Location = System::Drawing::Point(455, 525);
+			this->white_knight_r->Name = L"white_knight_r";
+			this->white_knight_r->Size = System::Drawing::Size(70, 70);
+			this->white_knight_r->TabIndex = 34;
+			this->white_knight_r->TabStop = false;
+			// 
+			// white_rook_r
+			// 
+			this->white_rook_r->BackColor = System::Drawing::Color::Transparent;
+			this->white_rook_r->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"white_rook_r.BackgroundImage")));
+			this->white_rook_r->Location = System::Drawing::Point(525, 525);
+			this->white_rook_r->Name = L"white_rook_r";
+			this->white_rook_r->Size = System::Drawing::Size(70, 70);
+			this->white_rook_r->TabIndex = 35;
+			this->white_rook_r->TabStop = false;
+			// 
 			// Game1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->ClientSize = System::Drawing::Size(830, 630);
+			this->Controls->Add(this->white_rook_r);
+			this->Controls->Add(this->white_knight_r);
+			this->Controls->Add(this->white_king);
+			this->Controls->Add(this->white_bishop_r);
+			this->Controls->Add(this->white_queen);
+			this->Controls->Add(this->white_bishop_l);
+			this->Controls->Add(this->white_knight_l);
+			this->Controls->Add(this->white_rook_l);
+			this->Controls->Add(this->white_pawn_8);
+			this->Controls->Add(this->white_pawn_7);
+			this->Controls->Add(this->white_pawn_6);
+			this->Controls->Add(this->white_pawn_5);
+			this->Controls->Add(this->white_pawn_4);
+			this->Controls->Add(this->white_pawn_3);
+			this->Controls->Add(this->white_pawn_2);
+			this->Controls->Add(this->white_pawn_1);
 			this->Controls->Add(this->black_pawn_8);
 			this->Controls->Add(this->black_pawn_7);
 			this->Controls->Add(this->black_pawn_6);
@@ -322,6 +548,22 @@ namespace KSGGames {
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->black_pawn_6))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->black_pawn_7))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->black_pawn_8))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_3))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_4))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_5))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_6))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_7))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_pawn_8))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_rook_l))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_knight_l))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_bishop_l))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_queen))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_bishop_r))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_king))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_knight_r))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->white_rook_r))->EndInit();
 			this->ResumeLayout(false);
 
 		}
