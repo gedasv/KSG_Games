7193ca4792ae9d2786f4d3ab8cfd156ee70a8597
MyRuonis <kciulcinskas@gmail.com>
2019-04-18 19:46:50 +0300

    lel

--- CIA SU ZMOGUM CHESS/ChessPrac.cpp
+++ CIA SU ZMOGUM CHESS/ChessPrac.cpp
@@ -1,13 +1,1 @@
 #include "ChessPrac.h"
-
-using namespace System;
-using namespace System::Windows::Forms;
-
-[STAThread]
-int main(array<System::String ^> ^args)
-{
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	ChessPrac::ChessPrac form;
-	Application::Run(%form);
-}
